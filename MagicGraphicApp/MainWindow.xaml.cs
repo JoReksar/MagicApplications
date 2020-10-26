05a981f44116b6c326b6973dac472a87383f38cb
Nikita@ <kokin.ru@gmail.com>
2020-10-26 20:24:47 +0300

    Useless Commit

--- MagicGraphicApp/MainWindow.xaml.cs
+++ MagicGraphicApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,51 @@ namespace MagicGraphicApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadImage() 
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Filter = "|*.jpg||*.png"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                Uri URI = new Uri(dialog.FileName, UriKind.Absolute);
+                ImageSource source = new BitmapImage(URI);
+                Image image = new Image()
+                {
+                    Source = source
+                };
+
+                var imageWindow = new WindowWithImage(image);
+                imageWindow.Show();
+            }
+        }
+        private void LoadRectangle()
+        {
+
+        }
+
+        private void LoadEllipse()
+        {
+
+        }
+
+        private void LoadLine()
+        {
+
+        }
+
+        private void LoadPolygon()
+        {
+
+        }
+
+        private void ShowImage(object sender, RoutedEventArgs e)
+        {
+            LoadImage();
         }
     }
 }
---
+++ MagicGraphicApp/WindowWithImage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MagicGraphicApp
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowWithImage.xaml
+    /// </summary>
+    public partial class WindowWithImage : Window
+    {
+        public WindowWithImage(Image image)
+        {
+            InitializeComponent();
+            this.image = image;
+            image.Width = 1000;
+            image.Height = 1000;
+            //mainCanvas.Children.Add(image);
+        }
+    }
+}
